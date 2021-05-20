fa42d82ddd6091b170b65c5b0a5dc3afb46d6987
TheGingeraffe <hestonio@gmail.com>
2021-05-20 15:46:47 -0700

    Testing NexusMods login

--- selenium_test.py
+++ selenium_test.py
@@ -4,14 +4,32 @@
 from selenium import webdriver
 import time
 
+login_url = "https://users.nexusmods.com/auth/sign_in"
 mod_url = "https://www.nexusmods.com/morrowind/mods/19510?tab=files&file_id=1000007846"
 
+# Set and ask for WebDriver
 # FF WebDriver https://github.com/mozilla/geckodriver/releases
 # Chrome WebDriver https://sites.google.com/a/chromium.org/chromedriver/downloads
 
-browser = webdriver.Chrome()
-browser.get(mod_url)
+driver = webdriver.Chrome()
 
-# click Slow Download
-slow_dl_button = browser.find_elements_by_xpath("//button[@id='slowDownloadButton']")[0]
-slow_dl_button.click()
+# Logging into nexusmods.com
+
+def site_login(login_url, username, password):
+    driver.get(login_url)
+    driver.find_element_by_id("user_login").send_keys(username)
+    driver.find_element_by_id("password").send_keys(password)
+    driver.find_element_by_name("commit").click()
+
+username = input("NexusMods username?: ")
+password = input("NexusMods password?: ")
+
+site_login(login_url, username, password)
+
+# Downloading mods
+
+def mod_dl(mod_url)
+    driver = webdriver.Chrome()
+    driver.get(mod_url)
+    slow_dl_button = browser.find_elements_by_xpath("//button[@id='slowDownloadButton']")[0]
+    slow_dl_button.click()
