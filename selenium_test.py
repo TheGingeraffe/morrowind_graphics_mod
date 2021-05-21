d2faabc9662299acb7d7335668a1760c98e32cbf
TheGingeraffe <hestonio@gmail.com>
2021-05-21 08:22:02 -0700

    Added sleep

--- selenium_test.py
+++ selenium_test.py
@@ -18,6 +18,7 @@ def site_login(login_url, username, password):
     driver.find_element_by_id("user_login").send_keys(username)
     driver.find_element_by_id("password").send_keys(password)
     driver.find_element_by_name("commit").click() 
+    time.sleep(10)
     driver.find_element_by_class("d-none d-md-flex").click()
     time.sleep(10)
     driver.get(mod_url)
