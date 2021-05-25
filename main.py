3505d9c39318e44d29a8fc7ce0e5e10ae43d2672
TheGingeraffe <hestonio@gmail.com>
2021-05-25 11:01:45 -0700

    Testing file id route again

--- main.py
+++ main.py
@@ -112,8 +112,9 @@ def mod_dl(mod_url):
             mod_files = driver.find_element_by_id("mod_files")
             download_button = mod_files.find_element_by_xpath("//span[text()='Manual download']")
             download_button.click()
-            slow_download_button = driver.find_element_by_xpath("//span[text()='Slow download']")
-            slow_download_button.click()
+            file_id = driver.current_url.split("file_id=")
+            driver.get("https://www.nexusmods.com/Core/Libs/Common/Widgets/DownloadPopUp?id=" + file_id[1] + "&game_id=100&source=FileExpander")
+ 
     elif "modhistory" in mod_url:
         download_button = driver.find_element_by_id("dlb")
         download_button.click()
